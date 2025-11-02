4a08fe549ab1bc52e3f54c1482b66380f017a83a
jahatef <jahatef@gmail.com>
2025-11-02 12:19:12 -0500

    saving changes

--- run_training.sh
+++ run_training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -t 24:0:00
-#SBATCH -N 4
+#SBATCH -t 1:0:00
+#SBATCH -N 1
 #SBATCH -p gpu
 #SBATCH -A PZS0622
 #SBATCH --ntasks-per-node=4
@@ -21,7 +21,47 @@ OMP_NUM_THREADS=12 torchrun --nproc_per_node=4  train_4k_fsdp.py \
   --min-lr 0.00000001 \
   --cpt-power 2 \
   --num-workers 8 \
+  --output-dir ./checkpoints-corn-from-384-gbs-48/ \
+  --use-wandb \
+  --resume ./checkpoints-corn-from-384-gbs-48/vit_large_patch16_384_epoch15.pt
+  #--resume /users/PAS2312/jahatef/ascend/vrwkv/high-res-training/checkpoints-stage3-ablate/vit_large_patch16_384_epoch1.pt
+
+OMP_NUM_THREADS=12 torchrun --nproc_per_node=4  train_4k_fsdp.py \
+  --data-dir /fs/ess/PAS2699/jahatef/corn/ \
+  --epochs 15 \
+  --batch-size 3 \
+  --mixed-precision \
+  --lr 0.000001 \
+  --min-lr 0.00000001 \
+  --cpt-power 2 \
+  --num-workers 8 \
   --output-dir ./checkpoints-corn-from-pretrained-ablate/ \
   --use-wandb \
-  --resume /users/PAS2312/jahatef/cardinal/high-res/high-res-training/checkpoints-stage3-ablate/vit_large_patch16_384_epoch4.pt
+  --resume ./checkpoints-corn-from-pretrained-ablate/vit_large_patch16_384_epoch14.pt
+
+  OMP_NUM_THREADS=12 torchrun --nproc_per_node=4  train_4k_fsdp.py \
+  --data-dir /fs/ess/PAS2699/jahatef/soybeans/ \
+  --epochs 15 \
+  --batch-size 3 \
+  --mixed-precision \
+  --lr 0.000001 \
+  --min-lr 0.00000001 \
+  --cpt-power 2 \
+  --num-workers 8 \
+  --output-dir ./checkpoints-soybeans-from-384-gbs-48/ \
+  --use-wandb \
+  --resume ./checkpoints-soybeans-from-384-gbs-48/vit_large_patch16_384_epoch15.pt
   #--resume /users/PAS2312/jahatef/ascend/vrwkv/high-res-training/checkpoints-stage3-ablate/vit_large_patch16_384_epoch1.pt
+
+OMP_NUM_THREADS=12 torchrun --nproc_per_node=4  train_4k_fsdp.py \
+  --data-dir /fs/ess/PAS2699/jahatef/soybeans/ \
+  --epochs 15 \
+  --batch-size 3 \
+  --mixed-precision \
+  --lr 0.000001 \
+  --min-lr 0.00000001 \
+  --cpt-power 2 \
+  --num-workers 8 \
+  --output-dir ./checkpoints-soybeans-from-pretrained-ablate/ \
+  --use-wandb \
+  --resume ./checkpoints-soybeans-from-pretrained-ablate/vit_large_patch16_384_epoch15.pt
